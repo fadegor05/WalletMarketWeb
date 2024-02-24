d137a5f8b22449c3995352f8788c3f037bf91c17
fadegor05 <fad.egor05@gmail.com>
2024-02-25 00:13:40 +0400

    added nginx, update readme, small content fixes

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 Web application that makes your life easier when you need to search for skins in MarketCSGO to deposit money into your Steam account. **Qiwi has blocked in Russia recently, so this is one of the only remaining methods.**
 
-[![Screenshot](/images/image-1.png)]()
+[![Screenshot](/images/image-1.png)](http://wm.fadegor05.ru/)
 
-[Website]()
+[Website](http://wm.fadegor05.ru/)
 
 ## Stack & technologies
 
--- src/components/Title.vue
+++ src/components/Title.vue
@@ -3,7 +3,7 @@
         <div class="font-inter mx-auto text-4xl mt-10 font-black text-white text-center">
             WalletMarket
         </div>
-        <a href="https://github.com/fadegor05/WalletMarket" class="font-inter mx-auto text-xl text-center font-bold bg-gradient-to-r text-transparent bg-clip-text from-blue-500 via-pink-500 to-blue-500 opacity-90 hover:opacity-100 transition-all">
+        <a href="https://github.com/fadegor05/WalletMarketWeb" class="font-inter mx-auto text-xl text-center font-bold bg-gradient-to-r text-transparent bg-clip-text from-blue-500 via-pink-500 to-blue-500 opacity-90 hover:opacity-100 transition-all">
             by fadegor05
         </a>
     </div>
